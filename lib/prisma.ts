465f1059a364e69ed3f782d40ddf870763d8405b
Sanele Hlongwane <sanelehlongwane61@gmail.com>
2024-07-11 06:27:32 +0200

    fixed some errors again

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,22 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+
+// Prevent multiple instances of Prisma Client in development
+let prisma: PrismaClient;
 
 declare global {
-    var prisma: PrismaClient | undefined
+  // eslint-disable-next-line no-var, no-unused-vars
+  var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient()
-
-if(process.env.NODE_ENV !== 'production'){
-    globalThis.prisma = db
+if (typeof window === 'undefined') {
+  if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+  } else {
+    if (!global.prisma) {
+      global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
+  }
 }
+
+export const db = prisma;
